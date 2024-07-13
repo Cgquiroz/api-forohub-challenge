687a5ddd656a23dc3fa3b09565712f802296878f
Carlos Ibarra <carlosibarra0411@hotmail.com>
2024-07-13 11:11:04 -0700

    creacion del proyecto

---
+++ src/main/java/foro/hub/api_forohub_challenge/ApiForohubChallengeApplication.java
@@ -0,0 +1,13 @@
+package foro.hub.api_forohub_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForohubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForohubChallengeApplication.class, args);
+	}
+
+}
