7c37ebfe8b97007f2c0ffce6e3db8b11c22bc240
Carlos Ibarra <carlosibarra0411@hotmail.com>
2024-07-15 18:44:05 -0700

    Acces Control created

--- src/main/java/foro/hub/api_forohub_challenge/infra/security/TokenService.java
+++ src/main/java/foro/hub/api_forohub_challenge/infra/security/TokenService.java
@@ -3,6 +3,8 @@ package foro.hub.api_forohub_challenge.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import foro.hub.api_forohub_challenge.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,7 +37,29 @@ public class TokenService {
         }
     }
 
+    public String getSubject(String token) {
+        if (token==null){
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);//validando la firma
+            verifier = JWT.require(algorithm)
+                    .withIssuer("foro hub")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null){
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
+    }
+
     private Instant generarFechaExpiracion(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
     }
+
 }
